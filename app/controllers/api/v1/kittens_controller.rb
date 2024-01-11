3c11d94b64a659d3957130d07f3c714f041c2487
dotaplayer1 <georgemao72@gmail.com>
2024-01-11 12:49:46 -0800

    Create API

--- app/controllers/api/v1/kittens_controller.rb
+++ app/controllers/api/v1/kittens_controller.rb
@@ -5,20 +5,40 @@ class Api::V1::KittensController < ApplicationController
   end
 
   def show
-  end
-
-  def new
+    @kitten = Kitten.find(params[:id])
+    render json: @kitten
   end
 
   def create
-  end
+    @kitten = Kitten.new(kitten_params)
 
-  def edit
+    if @kitten.save
+      render json: @kitten
+    else
+      render json: { message: "Creating cat failed", errors: @kitten.errors }, status: 400
+    end
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      render json: @kitten
+    else
+      render json: { message: "Updating cat failed", errors: @kitten.errors }, status: 400
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def kitten_params
+    params.permit(:name, :age, :cuteness, :softness)
   end
 end
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,14 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+
+  def as_json(options = nil)
+    {
+      id: self.id,
+      name: self.name,
+      age: self.age,
+      cuteness: self.cuteness,
+      softness: self.softness
+    }
+  end
 end
